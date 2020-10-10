181606e7d589dab9b443c5404e3d77fa5f204380
silviuilas <silviuilas@gmail.com>
2020-10-10 10:12:26 +0300

    recapcha invisible

--- submited_form.php
+++ submited_form.php
@@ -1,24 +1,28 @@
 <?php
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-  $fname=$_POST["firstname"];
-  $lname=$_POST['lastname'];
-  $phone=$_POST['phonenumber'];
-  $category=$_POST['category'];
-  $type = $_POST['type'];
-  $details = '';
-  if(isset($_POST['details']))
-    $details=$_POST['details'];
-  $msg = "Nume: ".$fname."\nPrenume: ".$lname."\nTelefon: ".$phone."\nCategorie: ".$category."\nTip: ".$type."\n Detalii: ".$details;
-  $msg = wordwrap($msg,70);
-  $subject= '[Inscriere] Elevul '.$fname.' '.$lname.' doreste sa se inscrie';
-  $from = 'From: scoaladesoferi.botosani.ro';
-
-  if(!mail("xxlnorddriver@gmail.com",$subject,$msg,$from)){
-    var_dump(error_get_last()['message']);
-    include('404.html');
+  print($_POST['g-recaptcha-response']);
+  if($_POST['g-recaptcha-response']) {
+    $fname = $_POST["firstname"];
+    $lname = $_POST['lastname'];
+    $phone = $_POST['phonenumber'];
+    $category = $_POST['category'];
+    $type = $_POST['type'];
+    $details = '';
+    if (isset($_POST['details']))
+      $details = $_POST['details'];
+    $msg = "Nume: " . $fname . "\nPrenume: " . $lname . "\nTelefon: " . $phone . "\nCategorie: " . $category . "\nTip: " . $type . "\n Detalii: " . $details;
+    $msg = wordwrap($msg, 70);
+    $subject = '[Inscriere] Elevul ' . $fname . ' ' . $lname . ' doreste sa se inscrie';
+    $from = 'From: scoaladesoferi.botosani.ro';
+    if (!mail("xxlnorddriver@gmail.com", $subject, $msg, $from)) {
+      var_dump(error_get_last()['message']);
+      include('404.html');
+    } else {
+      include('html/submited_form.html');
+    }
   }
-  else{
-    include ('html/submited_form.html');
+  else {
+    print("Something went wrong,try again");
   }
 }
